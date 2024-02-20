2ff2872233628996d3f55cf6568d35498e9325cf
Nathan Lenz <nathan@freelenzer.at>
2024-02-20 20:59:41 +0100

    Automatisches Scannen der Ordnerstruktur und Darstellung in swp3_xampp/index.php Namensaenderungen für strukturell besseren Überblick.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php-Teil</title>
+    <link href="2_Global-Chat/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-info-subtle">
+<h1 class="bg-info p-3 rounded-pill text-center w-50 m-auto mt-3 mb-3">PHP - Teil</h1>
+<?php
+$verzeichnis = scandir("./");
+for ($i = 4; $i < count($verzeichnis); $i++) {
+    if ($verzeichnis[$i] != "index.php") {
+        echo "<div class='bg-info p3 rounded-pill text-center w-25 m-auto mt-3 mb-3'>
+        <a class='text-white' href='$verzeichnis[$i]/index.php'>$verzeichnis[$i]</a>
+      </div>";
+    }
+}
+?>
+</body>
+</html>
